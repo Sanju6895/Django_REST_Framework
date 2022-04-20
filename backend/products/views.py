a308500d2fe05be6c774458b786936bcb1dd6f73
Sanjay Gupta <guptasanjay9091@gmail.com>
2022-04-20 18:47:11 +0530

    Implementing Mixins

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,8 +1,15 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 
 from .models import Product
 from .serializers import ProductSerializer
 
+
+"""
+We have 3 types of views here
+Function based
+Class based 
+Mixin based
+"""
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -33,3 +40,26 @@ class ProductDestroyAPIView(generics.DestroyAPIView):
         super().perform_destroy(instance)
 
 
+#This is mixin based view
+class ProductMixinView(mixins.ListModelMixin,
+                        mixins.RetrieveModelMixin,
+                        mixins.CreateModelMixin,
+                        mixins.DestroyModelMixin,
+                        generics.GenericAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    
--- py_client/create.py
+++ py_client/create.py
@@ -5,6 +5,6 @@ endpoint = "http://localhost:8000/api/products/"
 #api_response = requests.get(endpoint, params={"abc": 123}, json={"query":"Hello World"} ) #HTTP request
 
 api_response = requests.post(endpoint,
-        json={"title":"Spanish Giants","content":"You know its Real Madrid CF"} ) #HTTP request
+        json={"title":"Make it amazing","content":"Best company in the world"} ) #HTTP request
 
 print(api_response.json())
